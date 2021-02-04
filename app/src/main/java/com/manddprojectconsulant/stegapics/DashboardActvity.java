1ceab6ef7b950ae63a38caeeff606077a98db41c
dev9141 <devnesss@gmail.com>
2021-02-04 13:48:36 +0530

    Permission architecture change and dashboard async

--- app/src/main/java/com/manddprojectconsulant/stegapics/DashboardActvity.java
+++ app/src/main/java/com/manddprojectconsulant/stegapics/DashboardActvity.java
@@ -1,9 +1,12 @@
 package com.manddprojectconsulant.stegapics;
 
 import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
@@ -24,8 +27,14 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import static android.Manifest.permission.CAMERA;
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+import static android.Manifest.permission.RECORD_AUDIO;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
 public class DashboardActvity extends AppCompatActivity {
 
+    public static final int RequestPermissionCode = 7;
     RecyclerView rvencryptedlist;
     FloatingActionButton fabencryption, fabdecryption, fabshare, fabrate;
 
@@ -39,11 +48,24 @@ public class DashboardActvity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard_actvity);
 
-        checkAndRequestPermissions();
         init();
 
+        //Checking App Permission Granted or Not
+        if (CheckingPermissionIsEnabledOrNot()) {
+            //Entered in if Permission is granted
+
+            new LoadImages(DashboardActvity.this).execute();
+        }
+        // If, If permission is not enabled then else condition will execute.
+        else {
+            //Calling method to enable permission.
+            RequestMultiplePermission();
+        }
+
+
         //Please not consider it showing the list view how it looks for design purpose
-        ListShow();
+        //ListShow();
+        new LoadImages(DashboardActvity.this).execute();
 
 
         fabencryption.setOnClickListener(new View.OnClickListener() {
@@ -90,46 +112,70 @@ public class DashboardActvity extends AppCompatActivity {
         });
     }
 
-    public void checkAndRequestPermissions() {
-        int permissionWriteStorage = ContextCompat.checkSelfPermission(this,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE);
-        int ReadPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
-        int CameraPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+    public boolean CheckingPermissionIsEnabledOrNot() {
+        int FirstPermissionResult = ContextCompat.checkSelfPermission(getApplicationContext(), CAMERA);
+        int SecondPermissionResult = ContextCompat.checkSelfPermission(getApplicationContext(), READ_EXTERNAL_STORAGE);
+        int ThirdPermissionResult = ContextCompat.checkSelfPermission(getApplicationContext(), WRITE_EXTERNAL_STORAGE);
+        return FirstPermissionResult == PackageManager.PERMISSION_GRANTED &&
+                SecondPermissionResult == PackageManager.PERMISSION_GRANTED &&
+                ThirdPermissionResult == PackageManager.PERMISSION_GRANTED;
+    }
 
+    private void RequestMultiplePermission() {
+        // Creating String Array with Permissions.
+        ActivityCompat.requestPermissions(DashboardActvity.this, new String[]
+                {
+                        CAMERA,
+                        READ_EXTERNAL_STORAGE,
+                        WRITE_EXTERNAL_STORAGE
+                }, RequestPermissionCode);
+    }
 
-        List<String> listPermissionsNeeded = new ArrayList<>();
-        if (ReadPermission != PackageManager.PERMISSION_GRANTED) {
-            listPermissionsNeeded.add(Manifest.permission.READ_EXTERNAL_STORAGE);
-        }
-        if (permissionWriteStorage != PackageManager.PERMISSION_GRANTED) {
-            listPermissionsNeeded.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
-        }
-
-        if (CameraPermission != PackageManager.PERMISSION_GRANTED) {
-            listPermissionsNeeded.add(Manifest.permission.CAMERA);
-        }
-
-        if (!listPermissionsNeeded.isEmpty()) {
-            ActivityCompat.requestPermissions(this, listPermissionsNeeded.toArray(new String[0]), 1);
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case RequestPermissionCode:
+                if (grantResults.length > 0) {
+                    boolean CameraPermission = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    boolean ReadExternalStoragePermission = grantResults[1] == PackageManager.PERMISSION_GRANTED;
+                    boolean WriteExternalStoragePermission = grantResults[2] == PackageManager.PERMISSION_GRANTED;
+                    if (CameraPermission && ReadExternalStoragePermission && WriteExternalStoragePermission ) {
+                        //Toast.makeText(ActivityLogin.this, "Permission Granted", Toast.LENGTH_SHORT).show();
+                        //init();
+                        new LoadImages(DashboardActvity.this).execute();
+                    } else {
+                        finish();
+                        Toast.makeText(DashboardActvity.this, "Permission Denied", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                break;
         }
     }
 
-
-    private void ListShow() {
+    private String ListShow() {
         try {
             encryptedlists = new ArrayList<>();
             encryptedlists = getlist();
 
             Collections.reverse(encryptedlists);
-            encryptedlistAdapter = new EncryptedlistAdapter(encryptedlists, DashboardActvity.this);
+            /*encryptedlistAdapter = new EncryptedlistAdapter(encryptedlists, DashboardActvity.this);
             RecyclerView.LayoutManager mLayoutManger = new LinearLayoutManager(getApplicationContext());
             rvencryptedlist.setLayoutManager(mLayoutManger);
             rvencryptedlist.setItemAnimator(new DefaultItemAnimator());
             rvencryptedlist.setNestedScrollingEnabled(false);
             rvencryptedlist.setAdapter(encryptedlistAdapter);
+            encryptedlistAdapter.notifyDataSetChanged();*/
+
+            encryptedlistAdapter = new EncryptedlistAdapter(encryptedlists, DashboardActvity.this);
+            rvencryptedlist.setAdapter(encryptedlistAdapter);
+            rvencryptedlist.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+            rvencryptedlist.setItemAnimator(new DefaultItemAnimator());
+            rvencryptedlist.setNestedScrollingEnabled(false);
             encryptedlistAdapter.notifyDataSetChanged();
+            return "Success";
         } catch (Exception e) {
             e.printStackTrace();
+            return "Fail";
         }
     }
 
@@ -208,6 +254,61 @@ public class DashboardActvity extends AppCompatActivity {
         }
 
 
+    }
+
+    public class LoadImages extends AsyncTask<String, String, String>{
+
+        Context context;
+        ProgressDialog progressDialog;
+
+        public LoadImages(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressDialog = new ProgressDialog(context);
+            progressDialog.setMessage("Loading Images");
+            progressDialog.setCancelable(false);
+            progressDialog.show();
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            encryptedlists = new ArrayList<>();
+            encryptedlists = getlist();
+
+            if(encryptedlists.size() > 0) {
+                Collections.reverse(encryptedlists);
+                return "Success";
+            }
+            else {
+                return "NoImage";
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            if(progressDialog != null){
+                progressDialog.dismiss();
+            }
+
+            if(s.equals("Success")){
+                try {
+                    encryptedlistAdapter = new EncryptedlistAdapter(encryptedlists, DashboardActvity.this);
+                    rvencryptedlist.setAdapter(encryptedlistAdapter);
+                    rvencryptedlist.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+                    encryptedlistAdapter.notifyDataSetChanged();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            else {
+                Toast.makeText(context, "getting error while loading images", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 
 }
